08b49f5af56bf75cec8866d7ff4f125170aaa48d
no <109254246+Da-nu@users.noreply.github.com>
2024-02-15 20:58:16 -0600

    pre-assigned

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -5,6 +5,38 @@ import { validationResult } from 'express-validator';
 
 const saltRounds = 10 //for when a user signs up
 
+export const createUser = async (req: Request, res: Response) => {
+    // Check for validation errors
+    console.log("Req.body", req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log("errors", errors);
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Proceed with creating the User object
+    try {
+        const name = req.body.name as string;
+        const email = req.body.email as string;
+        const password = req.body.password as string;
+
+        let passwordHash: Promise<string>;
+        passwordHash = hash(password, saltRounds);
+
+        const user = new User({
+            name: name,
+            email: email,
+            passwordHash: passwordHash,
+            emailConfirmed: true,
+            emailConfirmationCode: "22222"
+        });
+        await user.save();
+        res.status(201).json({user});
+    } catch (error: any) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
 export const loginUser = async (req: Request, res: Response) => {
 
     console.log("Req.body", req.body);
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { loginUser } from '../controllers/user-controller';
+import { loginUser, createUser } from '../controllers/user-controller';
 import { body } from 'express-validator';
 
 const router = Router()
@@ -8,5 +8,11 @@ router.post('/login',[
     body('email').isEmail().withMessage('Email must be valid').notEmpty().withMessage('Email is required'),
     body('password').isString().withMessage('Password must be string').notEmpty().withMessage('Password is required')
 ], loginUser)
+
+router.post('/signup',[
+    body('name').isString().withMessage('Name must be a string').notEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Email must be valid').notEmpty().withMessage('Email is required'),
+    body('password').isString().withMessage('Password must be string').notEmpty().withMessage('Password is required')
+], createUser)
 
 export default router;
