59a1695be5d9affecfdfecf100cb5e4706a9b6d0
AAlchi <alialchi07@gmail.com>
2024-02-19 00:56:17 -0600

    First Commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import dummyRoutes from "./routes/dummy-routes";
 import authRoutes from "./routes/auth-routes";
+import imageRoutes from "./routes/image-routes"
 
 const app: Application = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/api/dummies', dummyRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/items', imageRoutes)
 
 // ============================
 
