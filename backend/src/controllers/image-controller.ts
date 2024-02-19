ce9c585292b842358a9789d86fd68aa8f7e55e16
AAlchi <alialchi07@gmail.com>
2024-02-19 13:47:56 -0600

    Final Commit

--- backend/src/controllers/image-controller.ts
+++ backend/src/controllers/image-controller.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from 'express'; 
 import { validationResult } from 'express-validator';
 import Image from '../models/image';
-import Item from '../models/item';
-import User from '../models/user';
-import Dummy from '../models/dummy';
+import Item from '../models/item'; 
+import mongoose from 'mongoose';
 
 export const createImage = async (req: Request, res: Response) => {
-    // Check for validation errors
-    console.log("Req.body", req.body);
+    // Check for validation errors 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         console.log("errors", errors);
@@ -20,59 +18,72 @@ export const createImage = async (req: Request, res: Response) => {
         const isPrimary = req.body.isPrimary as boolean
         const order = req.body.order as number
 
-        const findItem = await Item.find({ _id: itemId }).catch(err => { return res.status(400).json(`Bad Request: ${err}`) })
-        Image.find().then(data => {
-            if (isPrimary) { 
-                if (data.length > 0) { 
-                    Image.updateMany({ isPrimary: true }, { isPrimary: false }, {
-                        new: true,
-                        upsert: true,
-                    }).catch(err => { return res.status(400).json(`Bad Request: ${err}`) })
-                }
-            } 
-        }).catch(err => {
-            return res.status(400).json(`Bad Request: ${err}`)
+        const isIdValid = mongoose.Types.ObjectId.isValid(itemId)
+
+        if (!isIdValid) {
+            return res.status(500).json({ message: "Invalid Image Id" }) 
+        }
+
+        const findItem = await Item.findOne({ _id: itemId }).catch(err => { 
+            return res.status(500).json({ message: "Server Error", error: err.message }) 
         }) 
-        console.log(findItem)
+
+        const findImage = await Image.findOne({ isPrimary: true }).catch(err => { 
+            return res.status(500).json({ message: "Server Error", error: err.message }) 
+        }) 
+
+        if (isPrimary && findImage) {
+            Image.findOneAndUpdate({ itemId: itemId, isPrimary: true }, { isPrimary: false }, {
+                new: true,
+                upsert: true
+            }).catch(err => { 
+                return res.status(500).json({ message: "Server Error", error: err.message }) 
+            }) 
+        }
+
         if (!findItem) {
-            res.status(404).json("Item Not Found")
-        } 
+            return res.status(404).json("Item Not Found")
+        }
 
-        // const image = new Image({
-        //     itemId: itemId,
-        //     imageUrl: imageUrl,
-        //     isPrimary: isPrimary,
-        //     order: order
-        // });
+        const image = new Image({
+            itemId: itemId,
+            imageUrl: imageUrl,
+            isPrimary: isPrimary,
+            order: order
+        });
 
-        // await image.save();
-        // res.status(201).json({ image });
+        image.save();
+        return res.status(201).json(image);
     } catch (error: any) {
-        res.status(500).json({ message: 'Server error', error: error.message });
+        return res.status(500).json({ message: 'Server error', error: error.message });
     }
 };
 
 export const getImages = async (req: Request, res: Response) => {
     try {
         const itemId = req.params.itemId
+        const isIdValid = mongoose.Types.ObjectId.isValid(itemId)
+
+        if (!isIdValid) {
+            return res.status(500).json({ message: "Invalid Item Id" }) 
+        }
 
         const checkImage = await Image.find({ itemId }).catch(err => {
-            return res.status(400).json(`Bad Request: ${err}`)
+            return res.status(500).json({ message: "Server Error", error: err.message }) 
         });
 
         if (!checkImage) {
-            return res.status(404).json("Image Not Found")
+            return res.status(404).json({ message: "Image Not Found" }) 
         }
 
-        res.status(200).json(checkImage);
+        return res.status(200).json(checkImage);
     } catch (error: any) {
-        res.status(500).json({ message: 'Server error', error: error.message });
+        return res.status(500).json({ message: 'Server error', error: error.message });
     }
 }
 
 export const putImage = async (req: Request, res: Response) => {
-    // Check for validation errors
-    console.log("Req.body", req.body);
+    // Check for validation errors 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         console.log("errors", errors);
@@ -84,13 +95,17 @@ export const putImage = async (req: Request, res: Response) => {
         const imageUrl = req.body.imageUrl as string;
         const isPrimary = req.body.isPrimary as boolean;
         const order = req.body.order as number;
+        const isIdValid = mongoose.Types.ObjectId.isValid(imageId)
 
+        if (!isIdValid) {
+            return res.status(500).json({ message: "Invalid Image Id" }) 
+        }
         const checkImage = await Image.find({_id: imageId}).catch(err => {
-            return res.status(400).json(`Bad Request: ${err}`)
+            return res.status(500).json({ message: "Server Error", error: err.message }) 
         })
 
         if (!checkImage) {
-            return res.status(404).json("Image Not Found") 
+            return res.status(404).json({ message: "Image Not Found" }) 
         } 
 
         const updateImageDetails = {
@@ -99,23 +114,40 @@ export const putImage = async (req: Request, res: Response) => {
             order: order
         }
 
-        const updateImage = Image.findOneAndUpdate({_id: imageId}, updateImageDetails, {
+        const updateImage = await Image.findOneAndUpdate({_id: imageId}, updateImageDetails, {
             new: true,
             upsert: true,
             includeResultMetadata: true
         }).catch(err => {
-            return res.status(400).json(`Bad Request: ${err}`)
+            return res.status(500).json({ message: "Server Error", error: err.message }) 
         })
 
-        res.status(201).json({ updateImage });
+        
+        Image.findOne({ _id: imageId }).then(data => {
+            if (!data) {
+                return res.status(404).json({ message: "Image Not Found" }) 
+            }
+            Image.findOne({ itemId: data.itemId }).then(newData => {
+            if (newData) { 
+                Image.findOneAndUpdate({ _id: newData?._id }, { isPrimary: true }, {
+                    new: true,
+                    upsert: true
+                }).catch(err => { 
+                    return res.status(500).json({ message: "Server Error", error: err.message }) 
+                })
+            }
+            })
+        })  
+
+        
+        return res.status(201).json({ updateImage });
     } catch (error: any) {
-        res.status(500).json({ message: 'Server error', error: error.message });
+        return res.status(500).json({ message: 'Server error', error: error.message });
     }
-}; 
+};  
 
 export const deleteImage = async (req: Request, res: Response) => {
-    // Check for validation errors
-    console.log("Req.body", req.body);
+    // Check for validation errors 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         console.log("errors", errors);
@@ -123,11 +155,32 @@ export const deleteImage = async (req: Request, res: Response) => {
     }
  
     try {
-        const imageId = req.params.imageId as string;
-        
-        const image = Image.findOneAndDelete({_id: imageId}).catch(err => { return res.status(400).json(`Bad Request ${err}`) })
-        res.status(201).json({ image });
+        const imageId = req.params.imageId as string; 
+        const isIdValid = mongoose.Types.ObjectId.isValid(imageId)
+
+        if (!isIdValid) {
+            return res.status(500).json({ message: "Invalid Image Id" }) 
+        }  
+  
+        Image.findOneAndDelete({_id: imageId}).then(data => {
+            if (!data) {
+                return res.status(404).json({ message: "Image Not Found" }) 
+            }
+            Image.findOne({ itemId: data.itemId }).then(newData => {
+                if (newData) { 
+                    Image.findOneAndUpdate({ _id: newData?._id }, { isPrimary: true }, {
+                        new: true,
+                        upsert: true
+                    }).catch(err => { 
+                        return res.status(500).json({ message: "Server Error", error: err.message }) 
+                    })
+                }
+            }) 
+            return res.status(201).json(data);
+        }).catch(err => { 
+            return res.status(500).json({ message: "Server Error", error: err.message }) 
+        })   
     } catch (error: any) {
-        res.status(500).json({ message: 'Server error', error: error.message });
+        return res.status(500).json({ message: 'Server error', error: error.message });
     }
 };
