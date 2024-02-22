4417afa04f8927236a442f1c55a6c12e08b3b566
no <109254246+Da-nu@users.noreply.github.com>
2024-02-21 19:21:33 -0600

    hi i have added tokens (kinda) and stuff (only for loign for now)

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -2,8 +2,13 @@ import {Request, response, Response} from 'express';
 import User from '../models/user';
 import { hash, compare } from 'bcrypt'
 import { validationResult } from 'express-validator';
-// import { createTransport } from "nodemailer" //unimplemented for now cause difficultires
+import * as jwt from "jsonwebtoken"
 
+import dotenv from "dotenv";
+dotenv.config();
+
+// import { createTransport } from "nodemailer" //unimplemented for now cause difficultires
+const tokenSECRET = process.env.TOKEN_SECRET as string;
 const saltRounds = 10 //for when a user signs up
 
 /*const transporter = createTransport(sendgridTransport({
@@ -11,6 +16,10 @@ const saltRounds = 10 //for when a user signs up
         api_key: process.env.SENDGRID_TOKEN
     }
 }))*/
+
+const generateAccessToken = (userId: string, email: string, time: string) => { // this is like this incase its used for extra things
+    return jwt.sign({email: email}, tokenSECRET, {expiresIn: time})
+}
 
 export const createUser = async (req: Request, res: Response) => {
     // Check for validation errors
@@ -57,8 +66,12 @@ export const loginUser = async (req: Request, res: Response) => {
         const password = req.body.password as string;
         User.findOne({email: email})
             .then(user => {
-                compare(password, user!.passwordHash/*forgot how to fix possibly null*/).then(result => {
-                    res.status(200).json({ message: 'Success', isPasswordCorrect: result });
+                if (!user) {
+                    return
+                }
+                compare(password, user.passwordHash/*forgot how to fix possibly null*/).then(result => {
+                    const userToken = generateAccessToken(user._id, email, "1 day")
+                    res.status(200).json({ token: userToken });
                 }).catch (error => {
                     res.status(500).json({ message: 'Server error', error: error.message });
                 })
