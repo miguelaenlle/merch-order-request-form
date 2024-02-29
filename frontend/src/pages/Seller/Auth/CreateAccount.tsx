325b155684a782983f53100df2b54e7339f95335
wschroed1 <wschroeder5332@stu.d214.org>
2024-02-29 16:46:22 -0600

    Added orange gradient, replaced br tags with margin-top, connected the inputs with onChange, fixed useState variable names, changed the href

--- frontend/src/pages/Seller/Auth/CreateAccount.tsx
+++ frontend/src/pages/Seller/Auth/CreateAccount.tsx
@@ -4,18 +4,32 @@ import { Input } from '@chakra-ui/react';
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import {useState} from "react";
 import { Link } from '@chakra-ui/react'
+import orangeGradient from'../../../assets/orangeGradient.png';
 import { ExternalLinkIcon } from '@chakra-ui/icons'
-// @ts-ignore
+
 const CreateAccount: React.FC<{}> = (props) => {
-   const [fullName, updatedFullName] = useState('');
-   const [userName, updatedUserName] = useState('');
-   const [password, updatedPassword] = useState('');
+   const [fullName, setFullName] = useState('');
+   const [userName, setUserName] = useState('');
+   const [password, setPassword] = useState('');
+
+   function nameEntered() {
+       setFullName(target.value);
+   }
+
+    function userEntered() {
+        setUserName(target.value);
+    }
+
+    function passwordEntered() {
+        setPassword(target.value);
+    }
+
+
     return (
+
         <div className={styles.content}>
+
             <h1>
-                <br></br>
-                <br></br>
-                <br></br>
                 Create New Account
 
             </h1>
@@ -25,6 +39,7 @@ const CreateAccount: React.FC<{}> = (props) => {
                     placeholder='Full Name'
                     size='md'
                     width='large'
+                    onChange={nameEntered}
                 />
             </div>
             <div>
@@ -33,33 +48,35 @@ const CreateAccount: React.FC<{}> = (props) => {
                        placeholder='Username'
                        size='md'
                        width='large'
+                       onChange={userEntered}
                 />
             </div>
+
             <div>
+
                 <Input
-                       value={password}
-                       placeholder='Password'
-                       size='md'
-                       width='large'/>
+                    value={password}
+                    placeholder='Password'
+                    size='md'
+                    width='large'
+                    onChange={passwordEntered}
+                />
+
             </div>
+
             <div>
                 <Button colorScheme='blue'>Sign Up</Button>
             </div>
+
             <div className={styles.link}>
-
-            <Link color='blue.300' href='http://localhost:5173/seller-login'>
-
-                <p>back to login</p>
+            <Link color='blue.300' href='/seller-login'>
+                <p className={styles.link}>Back to login</p>
             </Link>
                 </div>
-            <body>
-            <div className={styles.gradient}>
 
+            <div className={styles.container}>
+                <img src={orangeGradient} alt="gradient" className={styles.image}/>
             </div>
-            </body>
-
-
-
         </div>
 
 
