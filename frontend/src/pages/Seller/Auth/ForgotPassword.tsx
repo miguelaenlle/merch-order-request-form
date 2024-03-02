abfe86ca0e7c72734f03766a808a00f5a65a5562
rishipatellll <rpatel4329@stu.d214.org>
2024-03-02 16:01:49 -0600

    Added seller login page with the necessary features.

--- frontend/src/pages/Seller/Auth/ForgotPassword.tsx
+++ frontend/src/pages/Seller/Auth/ForgotPassword.tsx
@@ -1,9 +1,61 @@
-import * as React from "react"
-const ForgotPassword: React.FC<{}> = (props) => {
+import * as React from "react";
+import { Input, Button, Heading, Box } from '@chakra-ui/react';
+
+interface ForgotPasswordProps {}
+
+const ForgotPassword: React.FC<ForgotPasswordProps> = () => {
+    const [newPassword, setNewPassword] = React.useState<string>('');
+    const [confirmNewPassword, setConfirmNewPassword] = React.useState<string>('');
+
+    const handleResetPassword = () => {
+        console.log('Resetting password with:', { newPassword, confirmNewPassword });
+    };
+
     return (
-        <div>
-            <h1>Forgot password</h1>
-        </div>
+        <Box p={4} maxW="md" mx="auto">
+            <Heading as="h2" size="xl" textAlign="center" mb={6}>
+                Forgot Password
+            </Heading>
+
+
+            <Input
+                placeholder="New Password"
+                type="password"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                mb={4}
+            />
+
+
+            <Input
+                placeholder="Confirm New Password"
+                type="password"
+                value={confirmNewPassword}
+                onChange={(e) => setConfirmNewPassword(e.target.value)}
+                mb={6}
+            />
+
+
+            <Button
+                colorScheme="blue"
+                onClick={handleResetPassword}
+                mb={4}
+                w="100%"
+            >
+                Reset Password
+            </Button>
+
+
+            <Button
+                onClick={() => {
+                    console.log('Navigate back to login');
+                }}
+                w="100%"
+            >
+                Back to Login
+            </Button>
+        </Box>
     );
-}
-export default ForgotPassword
+};
+
+export default ForgotPassword;
