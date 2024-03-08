47f81f567cd61fe5db56645022c6310623b6dcee
AAlchi <alialchi07@gmail.com>
2024-03-07 22:12:19 -0600

    Revision Changes

--- backend/src/controllers/filter-item-controller.ts
+++ backend/src/controllers/filter-item-controller.ts
@@ -4,6 +4,14 @@ import InventoryItem from '../models/inventory-item';
 import Item from '../models/item';
 import mongoose from 'mongoose';
 
+interface ItemInterface {
+    _id: string,
+    name: string,
+    description: string,
+    pickupLocation: string,
+    pickupTime: string
+}
+
 export const getFilteredInventoryItems = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -13,7 +21,7 @@ export const getFilteredInventoryItems = async (req: Request, res: Response) => 
 
     try {
         // Filter
-        let fetchedInventoryItems = {};
+        let fetchedInventoryItems: any = {};
         const itemOwner = req.query.itemOwner as string
         const size = req.query.size as string
         const name = req.query.name as string
@@ -23,47 +31,37 @@ export const getFilteredInventoryItems = async (req: Request, res: Response) => 
         const sortedBy = req.query.sortedBy as string
 
         if (itemOwner && size && name) {
-            let tempData: any[] = await Item.find({})
-            let tempDataObject: any;
-            if (!tempData) {
-                res.status(404).json({ message: "Not Found" })
-            }
-
-            tempData.forEach((data) => {
-                const isIdValid = mongoose.Types.ObjectId.isValid(data._id) 
-    
-                if (!isIdValid) {
-                    return res.status(500).json({ message: "Invalid Data" }) 
-                }
-                
-                if (data.name.toLowerCase().includes(name.toLowerCase()) && data.itemOwner.toLowerCase().includes(itemOwner.toLowerCase())) {
-                    tempDataObject = data._id
-                }  
-            })
-            fetchedInventoryItems = {itemId: tempDataObject, size: size, amount: {$gt: 1}}
-        } else if (itemOwner && !size && !name) {
-            let tempData: any[] = await Item.find({})
+            let tempData: ItemInterface[] = await Item.find({})
             let tempDataObject: any;
             if (!tempData) {
                 res.status(404).json({ message: "Not Found" })
             }
 
             tempData.forEach((data) => { 
-                const isIdValid = mongoose.Types.ObjectId.isValid(data._id) 
-    
-                if (!isIdValid) {
-                    return res.status(500).json({ message: "Invalid Data" }) 
-                }
-
-                if (data.itemOwner.toLowerCase().includes(itemOwner.toLowerCase())) {
+                
+                if (data.name.toLowerCase().includes(name.toLowerCase()) && data._id === itemOwner) {
                     tempDataObject = data._id
                 }  
             })
-            fetchedInventoryItems = {itemId: tempDataObject, amount: {$gt: 1}}
+            fetchedInventoryItems += {itemId: tempDataObject, amount: {$gt: 1}}
+        } else if (itemOwner && !size && !name) {
+            let tempData: ItemInterface[] = await Item.find({})
+            let tempDataObject: any;
+            if (!tempData) {
+                res.status(404).json({ message: "Not Found" })
+            }
+
+            tempData.forEach((data) => {  
+
+                if (data._id === itemOwner) {
+                    tempDataObject = data._id
+                }  
+            })
+            fetchedInventoryItems = {_id: tempDataObject, amount: {$gt: 1}}
         } else if (!itemOwner && size && !name) {
             fetchedInventoryItems = await InventoryItem.find({size: size})
         } else if (!itemOwner && !size && name) {
-            let tempData: any[] = await Item.find({})
+            let tempData: ItemInterface[] = await Item.find({})
             let tempDataObject: any;
             if (!tempData) {
                 res.status(404).json({ message: "Not Found" })
@@ -80,9 +78,9 @@ export const getFilteredInventoryItems = async (req: Request, res: Response) => 
                     tempDataObject = data._id
                 }  
             })
-            fetchedInventoryItems = {itemId: tempDataObject, amount: {$gt: 1}}
+            fetchedInventoryItems += {itemId: tempDataObject, amount: {$gt: 1}}
         } else if (itemOwner && name && !size) {
-            let tempData: any[] = await Item.find({})
+            let tempData: ItemInterface[] = await Item.find({})
             let tempDataObject: any;
             if (!tempData) {
                 res.status(404).json({ message: "Not Found" })
@@ -95,49 +93,37 @@ export const getFilteredInventoryItems = async (req: Request, res: Response) => 
                     return res.status(500).json({ message: "Invalid Data" }) 
                 }
                 
-                if (data.name.toLowerCase().includes(name.toLowerCase()) && data.itemOwner.toLowerCase().includes(itemOwner.toLowerCase())) {
+                if (data.name.toLowerCase().includes(name.toLowerCase()) && data._id === itemOwner) {
                     tempDataObject = data._id
                 }  
             })
             fetchedInventoryItems = {itemId: tempDataObject, amount: {$gt: 1}}
         } else if (itemOwner && !name && size) {
-            let tempData: any[] = await Item.find({})
+            let tempData: ItemInterface[] = await Item.find({})
             let tempDataObject: any;
             if (!tempData) {
                 res.status(404).json({ message: "Not Found" })
             }
 
-            tempData.forEach((data) => {
-                const isIdValid = mongoose.Types.ObjectId.isValid(data._id) 
-    
-                if (!isIdValid) {
-                    return res.status(500).json({ message: "Invalid Data" }) 
-                }
-                
-                if (data.itemOwner.toLowerCase().includes(itemOwner.toLowerCase())) {
+            tempData.forEach((data) => { 
+                if (data._id === itemOwner) {
                     tempDataObject = data._id
                 }  
             })
-            fetchedInventoryItems = {itemId: tempDataObject, size: size, amount: {$gt: 1}}
+            fetchedInventoryItems += {itemId: tempDataObject, amount: {$gt: 1}}
         } else if (!itemOwner && name && size) {
-            let tempData: any[] = await Item.find({})
+            let tempData: ItemInterface[] = await Item.find({})
             let tempDataObject: any;
             if (!tempData) {
                 res.status(404).json({ message: "Not Found" })
             }
 
-            tempData.forEach((data) => {
-                const isIdValid = mongoose.Types.ObjectId.isValid(data._id) 
-    
-                if (!isIdValid) {
-                    return res.status(500).json({ message: "Invalid Data" }) 
-                }
-                
+            tempData.forEach((data) => { 
                 if (data.name.toLowerCase().includes(name.toLowerCase())) {
                     tempDataObject = data._id
                 }  
             })
-            fetchedInventoryItems = {itemId: tempDataObject, size: size, amount: {$gt: 1}}
+            fetchedInventoryItems += {itemId: tempDataObject, amount: {$gt: 1}}
         }
 
         if (!fetchedInventoryItems) {
@@ -150,7 +136,7 @@ export const getFilteredInventoryItems = async (req: Request, res: Response) => 
         if (sortedBy == "price") {
             finalInventoryItems = await InventoryItem.find(fetchedInventoryItems).sort({price: 1})
         } else if (sortedBy == "name") {
-            let tempData: any[] = await Item.find({})
+            let tempData: ItemInterface[] = await Item.find({})
             if (!tempData) {
                 res.status(404).json({ message: "Not Found" })
             }
@@ -158,7 +144,7 @@ export const getFilteredInventoryItems = async (req: Request, res: Response) => 
             finalItems = await Item.find({}).sort({name: 1})
         }
 
-        res.status(200).json({finalInventoryItems, finalItems})
+        res.status(200).json({finalItems})
 
     } catch (error: any) {
         res.status(500).json({ message: 'Server error', error: error.message });
