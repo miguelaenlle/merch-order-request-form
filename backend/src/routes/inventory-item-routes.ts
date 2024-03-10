1cd19628f16936569db76a536c7ab978f64f736d
AAlchi <alialchi07@gmail.com>
2024-03-09 20:43:19 -0600

    Final Changes

--- backend/src/routes/inventory-item-routes.ts
+++ backend/src/routes/inventory-item-routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'; 
 import { createInventoryItem, getIndividualInventoryItem, getInventoryItems, patchInventoryItem } from '../controllers/inventory-item-controller';
+import { loginUser } from "../controllers/auth-controller"
 import { body, param, query } from 'express-validator';
 
 const router = Router({ mergeParams: true });
@@ -9,15 +10,15 @@ router.post('/', [
     body('size').isString().withMessage('Size must be a string').notEmpty().withMessage('Size primary is required'),
     body('price').isNumeric().withMessage('Price must be a number').notEmpty().withMessage('Price is required'),
     body('amount').isNumeric().withMessage('Amount must be a number').notEmpty().withMessage('Amount is required')
-], createInventoryItem); 
-router.get('/', getInventoryItems);
+], loginUser, createInventoryItem); 
+router.get('/', loginUser, getInventoryItems);
 router.get('/:inventoryItemId', [
     param('inventoryItemId').isString().withMessage('Inventory item id must be a string').notEmpty().withMessage('Inventory item id is required') 
-], getIndividualInventoryItem);
+], loginUser, getIndividualInventoryItem);
 router.patch('/', [
     query('itemId').isString().withMessage('Item id must be a string').notEmpty().withMessage('Item id is required'),
     body('updatedInventoryItems').isArray().withMessage('Updated inventory items must be an array') 
-], patchInventoryItem);
+], loginUser, patchInventoryItem);
 
 export default router;
 
