ca4e39716722ae32e3c8449577510d7bf623b3b0
miguelaenlle <bot.developer3@gmail.com>
2024-03-13 17:42:47 -0500

    Fixed minor UI issues

--- frontend/src/pages/Seller/Auth/CreateAccount.tsx
+++ frontend/src/pages/Seller/Auth/CreateAccount.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import styles from "./SellerRegister.module.css"
 import { Input } from '@chakra-ui/react'
 import { Button, ButtonGroup } from "@chakra-ui/react";
-import {useState} from "react";
+import { useState } from "react";
 import { Link } from '@chakra-ui/react'
 import orangeGradient from '../../../assets/orangeGradient.png';
 import { FaDoorOpen } from 'react-icons/fa';
@@ -27,62 +27,64 @@ const CreateAccount: React.FC<{}> = (props) => {
 
     return (
 
-        <div className={styles.content}>
-            <h1>
-                Create New Account
+        <div className={`${styles.content} flexbox`}>
+            <div className={`${styles.container} flexItem`}>
+                <h1>
+                    Create New Account
 
-            </h1>
-            <div>
-                <Input
-                    value={fullName}
-                    placeholder='Full Name'
-                    size='md'
-                    width='large'
-                    onChange={nameEntered}>
-                </Input>
+                </h1>
+                <div>
+                    <Input
+                        value={fullName}
+                        placeholder='Full Name'
+                        size='md'
+                        width='large'
+                        onChange={nameEntered}>
+                    </Input>
+                </div>
+
+                <div>
+                    <Input
+                        value={userName}
+                        placeholder='Username'
+                        size='md'
+                        width='large'
+                        onChange={userEntered}>
+                    </Input>
+                </div>
+
+                <div>
+                    <Input
+                        value={password}
+                        placeholder='Password'
+                        size='md'
+                        width='large'
+                        onChange={passwordEntered}>
+                    </Input>
+                </div>
+
+                <div>
+                    <ChakraProvider>
+                        <Button
+                            leftIcon={<FaDoorOpen />}
+                            colorScheme="blue"
+                            variant="solid"
+                        >
+                            Sign Up
+                        </Button>
+                    </ChakraProvider>
+
+                </div>
+
+                <div className={styles.link}>
+                    <Link color='blue.300' href='/seller-login'>
+                        <p className={styles.link}>Back to login</p>
+                    </Link>
+                </div>
             </div>
 
-            <div>
-                <Input
-                    value={userName}
-                    placeholder='Username'
-                    size='md'
-                    width='large'
-                    onChange={userEntered}>
-                </Input>
-            </div>
+            <div className="flexItem orangeGradient">
 
-            <div>
-                <Input
-                    value={password}
-                    placeholder='Password'
-                    size='md'
-                    width='large'
-                    onChange={passwordEntered}>
-                </Input>
-            </div>
-
-            <div>
-                <ChakraProvider>
-                    <Button
-                        leftIcon={<FaDoorOpen />}
-                        colorScheme="blue"
-                        variant="solid"
-                    >
-                        Sign Up
-                    </Button>
-                </ChakraProvider>
-
-            </div>
-
-            <div className={styles.link}>
-                <Link color='blue.300' href='/seller-login'>
-                    <p className={styles.link}>Back to login</p>
-                </Link>
-            </div>
-
-            <div className={styles.container}>
-                <img src={orangeGradient} alt='gradient' className={styles.image}></img>
             </div>
         </div>
     );
