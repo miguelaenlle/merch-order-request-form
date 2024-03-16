caa032e7381a4f43c8cf042ab451097e16421dcb
rishipatellll <rpatel4329@stu.d214.org>
2024-03-16 18:13:44 -0500

    Revised

--- frontend/src/pages/Seller/Dashboard/SellerSpiritwearAndInventory.tsx
+++ frontend/src/pages/Seller/Dashboard/SellerSpiritwearAndInventory.tsx
@@ -1,11 +1,24 @@
-import * as React from "react"
 import NewSpiritwearItem from "./NewSpiritwearItem";
-const SellerSpiritwearAndInventory: React.FC<{}> = (props) => {
+import { JHHS_SWEATER } from "./imagePaths";
+import "./styles.css";
+
+const SellerSpiritwearAndInventory = () => {
     return (
         <div className="subcontent">
-            <h3 className="header">My Spiritwear and Inventory</h3>
-            <NewSpiritwearItem />
+            <h3 className="header">My Spiritwear & Inventory</h3>
+            <div className="createNewItem">
+                <NewSpiritwearItem />
+            </div>
+            <div className="imageContainer">
+                <img className="itemImage" src={JHHS_SWEATER} alt="Husky Sweater" />
+            </div>
+            <div className="itemDetails">
+                <h4 className="itemName">Husky Sweater</h4>
+                <p className="cost">Price: $20.00</p>
+                <p className="school"> JHHS Merch</p>
+            </div>
         </div>
     );
-}
-export default SellerSpiritwearAndInventory
+};
+
+export default SellerSpiritwearAndInventory;
