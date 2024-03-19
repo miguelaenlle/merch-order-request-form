67c5a36cf7e4814aa90a87e6a0e82c5f66bc8125
rishipatellll <rpatel4329@stu.d214.org>
2024-03-18 22:04:20 -0500

    Revised

--- frontend/src/pages/Seller/Dashboard/SellerSpiritwearAndInventory.tsx
+++ frontend/src/pages/Seller/Dashboard/SellerSpiritwearAndInventory.tsx
@@ -1,21 +1,26 @@
+
 import NewSpiritwearItem from "./NewSpiritwearItem";
 import { JHHS_SWEATER } from "./imagePaths";
+import { Tag } from "@chakra-ui/react";
 import "./styles.css";
-
 const SellerSpiritwearAndInventory = () => {
+    const schoolName = "JHHS Merch";
+
     return (
         <div className="subcontent">
             <h3 className="header">My Spiritwear & Inventory</h3>
             <div className="createNewItem">
-                <NewSpiritwearItem />
+                <NewSpiritwearItem/>
             </div>
-            <div className="imageContainer">
+            <div>
                 <img className="itemImage" src={JHHS_SWEATER} alt="Husky Sweater" />
             </div>
-            <div className="itemDetails">
+            <div className="itemDetails" style={{ textAlign: "left" }}>
                 <h4 className="itemName">Husky Sweater</h4>
-                <p className="cost">Price: $20.00</p>
-                <p className="school"> JHHS Merch</p>
+                <p className="cost"> $20 </p>
+                <Tag size="md" variant="solid" colorScheme="gray">
+                    {schoolName}
+                </Tag>
             </div>
         </div>
     );
