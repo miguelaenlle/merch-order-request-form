372a25444bb0ede4585ed8c792a748af9fa2e9f3
no <109254246+Da-nu@users.noreply.github.com>
2024-04-06 22:35:30 -0500

    idk whats happening but api hooks is in flames

--- frontend/src/components/shared/hooks/use-api-hook.ts
+++ frontend/src/components/shared/hooks/use-api-hook.ts
@@ -85,7 +85,7 @@ export const useAPIHook = () => {
         const sampleBuyerEmail = "bot.developer3@gmail.com" // User to be deleted after testing
         const password = "123456$"
 
-        const response = await post("http://localhost:5000/api/v1/auth/login", {
+        const response = await post("http://localhost:3000/api/auth/login", {
             email: sampleBuyerEmail,
             password: password
         })
@@ -97,7 +97,7 @@ export const useAPIHook = () => {
         const sampleSellerEmail = "bot.developer4@gmail.com" // User to be deleted after testing
         const password = "123456$"
 
-        const response = await post("http://localhost:5000/api/v1/auth/login", {
+        const response = await post("http://localhost:3000/api/auth/login", {
             email: sampleSellerEmail,
             password: password
         })
--- frontend/src/pages/Seller/Dashboard/SellerGroups.tsx
+++ frontend/src/pages/Seller/Dashboard/SellerGroups.tsx
@@ -2,10 +2,24 @@ import * as React from "react"
 import { Group } from "../../../components/shared/types/Group";
 import GroupItem from "./GroupItem";
 import { PLACEHOLDER_GROUPS } from "../../../constants/placeholder-data";
+import { useAPIHook } from "../../../components/shared/hooks/use-api-hook";
 
 const SellerGroups: React.FC<{}> = (props) => {
-    const [groups, setGroups] = React.useState<Group[]>(PLACEHOLDER_GROUPS);
+    const [groups, setGroups] = React.useState<Group[]>([]);
     // TODO: Fetch groups from our API
+
+    const apiHook = useAPIHook();
+    const retrieveGroups = async () => {
+        const sellerToken = await apiHook.generateSellerToken();
+
+        const response = await apiHook.get(
+            `http://localhost:3000/api/groups/`,
+            sellerToken
+        );
+        console.log(response)
+        setGroups(response)
+    }
+    retrieveGroups()
 
     return (
         <div className="subcontent">
