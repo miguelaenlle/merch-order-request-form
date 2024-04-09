d4144b9cd57cb5ff19b8618cb9a4b72b3aaaa05e
Dhruv <dhruvthakorpatel05@gmail.com>
2024-04-08 19:32:45 -0500

    Connected items-controller.ts backend to SellerSpiritwearAndInventory.tsx frontend

--- frontend/src/pages/Seller/Dashboard/SellerSpiritwearAndInventory.tsx
+++ frontend/src/pages/Seller/Dashboard/SellerSpiritwearAndInventory.tsx
@@ -9,33 +9,17 @@ import { useAPIHook } from "../../../components/shared/hooks/use-api-hook.ts";
 const SellerSpiritwearAndInventory = () => {
     const apiHook = useAPIHook();
     const [spiritwearItems, setSpiritwearItems] = useState<Item[]>([
-        {
-            _id: "1",
-            name: "(DUMMY) Husky Sweater",
-            price: 25,
-            groupIdName: "JHHS"
-        },
-        {
-            _id: "2",
-            name: "(DUMMY) Husky T-Shirt",
-            price: 15,
-            groupIdName: "JHHS"
-        },
-        {
-            _id: "3",
-            name: " (DUMMY) Husky Hoodie",
-            price: 35,
-            groupIdName: "JHHS"
-        } // Remove the placeholder data when you confirm this works
+
     ]);
 
     const handleLoad = async () => {
         const sellerToken = await apiHook.generateSellerToken();
         try {
             const response = await apiHook.get(
-                'https://localhost:3000/api/my-items/',
+                'http://localhost:3000/api/items/my-items/',
                 sellerToken);
-            setSpiritwearItems(response.data);
+            console.log(response)
+            setSpiritwearItems(response as Item[]);
         } catch (error) {
             console.log('Server error', error);
         }
