7ea15a984458f8f0d4a9ff0dc077bfef8c8d9785
rishipatellll <rpatel4329@stu.d214.org>
2024-04-09 19:03:48 -0500

    Revised

--- frontend/src/pages/Buyer/LatestReleases.tsx
+++ frontend/src/pages/Buyer/LatestReleases.tsx
@@ -1,23 +1,27 @@
 
 
-import React, { useState } from "react";
+import React from "react";
 import SearchResultItem from "../../components/shared/SearchResultItem";
 import { Item } from "../../components/shared/types/Item";
 import { PLACEHOLDER_ITEMS } from "../../constants/placeholder-data";
+import "./LatestReleases.css";
 
 const LatestReleases: React.FC = () => {
-    const [items] = useState<Item[]>(PLACEHOLDER_ITEMS);
+    const items: Item[] = PLACEHOLDER_ITEMS;
 
     return (
-        <div>
-            <h1>Latest Releases</h1>
-            {items.map((item) => (
-                <SearchResultItem
-                    key={`search-result-item-${item._id}`}
-                    item={item}
-                />
-            ))}
+        <div className="latest-releases-container">
+            <h1 className="header">Latest Releases</h1>
+            <div className="grid-container">
+                {items.map((item) => (
+                    <SearchResultItem
+                        key={`search-result-item-${item._id}`}
+                        item={item}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
+
 export default LatestReleases;
