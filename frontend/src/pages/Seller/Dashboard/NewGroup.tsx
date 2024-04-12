38f6c906b48a80553da194ba57b9a26bbc92b0f7
wschroed1 <wschroeder5332@stu.d214.org>
2024-04-12 18:09:40 -0500

    Created input field, text, and create button

--- frontend/src/pages/Seller/Dashboard/NewGroup.tsx
+++ frontend/src/pages/Seller/Dashboard/NewGroup.tsx
@@ -1,15 +1,32 @@
 import * as React from "react"
 import CustomModal from "../../../components/shared/CustomModal";
+import { Input, Button } from '@chakra-ui/react'
+import {useState} from "react";
 const NewGroup: React.FC<{}> = (props) => {
+    const [name, setName] = useState('');
+    const nameEntered: React.ChangeEventHandler<HTMLInputElement> = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    };
     return (
         <div>
             <CustomModal
-                title="New Group"
                 buttonText="Create New Group"
                 content={
                     <div>
-                        <p>Content</p>
-                        <br />
+                        <br/>
+                        <strong style={{fontSize: "30px", fontWeight: "bold"}}>Create Group</strong>
+                        <Input
+                            placeholder='Name'
+                            marginTop='12px'
+                            value={name}
+                            onChange={nameEntered}
+                        />
+
+                        <div style={{paddingBottom: '15px'}}>
+                        <Button colorScheme='blue' marginTop="80px">
+                            Create new group
+                        </Button>
+                            </div>
                     </div>
                 }
                 onAction={() => { }}
