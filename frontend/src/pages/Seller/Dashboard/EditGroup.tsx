221dba2dcbf2d4feb46c3c79b76911bbf59f265f
wschroed1 <wschroeder5332@stu.d214.org>
2024-04-13 16:13:05 -0500

    Added buttons, input field, and text

--- frontend/src/pages/Seller/Dashboard/EditGroup.tsx
+++ frontend/src/pages/Seller/Dashboard/EditGroup.tsx
@@ -1,18 +1,44 @@
 import * as React from "react"
 import CustomModal from "../../../components/shared/CustomModal";
+import {useState} from "react";
+import { Input, Button } from '@chakra-ui/react'
 const EditGroup: React.FC<{}> = (props) => {
+
+    const [name, setName] = useState('');
+    const nameEntered: React.ChangeEventHandler<HTMLInputElement> = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    };
+
     return (
         <div>
             <CustomModal
-                title="Edit Group"
+
                 buttonText="Edit Group"
                 content={
                     <div>
-                        <p>Content</p>
-                        <br />
+                        <br/>
+                        <strong style={{fontSize: "30px", fontWeight: "bold"}}>Edit Group</strong>
+                        <Input
+                            placeholder='Name'
+                            marginTop='12px'
+                            value={name}
+                            onChange={nameEntered}
+                        />
+                        <div style={{paddingBottom: '15px'}}>
+                            <Button colorScheme='blue' marginTop="80px" width="100px">
+                                Save
+                            </Button>
+
+                            <Button colorScheme='red' marginTop="80px" width="100px" marginLeft="200">
+                                Delete
+                            </Button>
+                        </div>
                     </div>
+
+
                 }
-                onAction={() => { }}
+                onAction={() => {
+                }}
                 disableDefaultButtons
             />
         </div>
