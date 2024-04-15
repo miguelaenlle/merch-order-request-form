7848cc40977bd961d930b4ecf140e36d31f648d6
miguelaenlle <bot.developer3@gmail.com>
2024-04-15 18:38:38 -0500

    Added order count

--- frontend/src/components/shared/BuyerNavbar.tsx
+++ frontend/src/components/shared/BuyerNavbar.tsx
@@ -3,33 +3,46 @@ import { Button } from "@chakra-ui/react";
 import "./Navbar.css"
 import { useNavigate } from "react-router-dom";
 import { FaBox, FaStore } from "react-icons/fa";
-import { JHHS_LOGO} from "../../constants/placeholder-data.ts";
+import { JHHS_LOGO } from "../../constants/placeholder-data.ts";
 
 const BuyerNavbar: React.FC<{}> = (props) => {
     const navigate = useNavigate();
+
+
+    const [numOrders, setNumOrders] = React.useState<number>(0);
+
+    const handleLoad = async () => {
+
+    }
+
+    React.useEffect(() => {
+        handleLoad()
+    }, [])
 
     return (
         <div className="navbar">
             <h3 className="clickableText" onClick={() => {
                 navigate("/")
             }}>
-                <img src={JHHS_LOGO} alt="Husky Icon" className="image"/>
+                <img src={JHHS_LOGO} alt="Husky Icon" className="image" />
                 <strong>Hersey Spiritwear</strong>
             </h3>
             <div className="spacer"></div>
-            <Button
-                colorScheme="gray"
-                variant="ghost"
-                onClick={() => {
-                    // TODO: This should go to seller lgoin instead
-                    navigate("/my-orders")
+            {numOrders > 0 && (
+                <Button
+                    colorScheme="gray"
+                    variant="ghost"
+                    onClick={() => {
+                        // TODO: This should go to seller lgoin instead
+                        navigate("/my-orders")
 
-                }}
-                fontSize={"sm"}
-            >
-                <FaBox style={{ marginRight: "5px" }} /> Orders
-                <div className="orderCount">1</div>
-            </Button>
+                    }}
+                    fontSize={"sm"}
+                >
+                    <FaBox style={{ marginRight: "5px" }} /> Orders
+                    <div className="orderCount">{numOrders}</div>
+                </Button>
+            )}
             <Button
                 colorScheme="gray"
                 variant="ghost"
@@ -44,7 +57,7 @@ const BuyerNavbar: React.FC<{}> = (props) => {
             </Button>
             <Button
                 colorScheme="gray"
-                 variant="ghost"
+                variant="ghost"
                 onClick={() => {
                     // TODO: This should go to seller lgoin instead
                     navigate("/seller-dashboard")
