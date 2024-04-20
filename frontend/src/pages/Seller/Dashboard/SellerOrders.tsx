b8ff22a3a29da997b99af398a5edcf58d4264a96
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-04-19 23:09:11 -0500

    Query parameters to filter by ID; Orders & order-item rendering

--- frontend/src/pages/Seller/Dashboard/SellerOrders.tsx
+++ frontend/src/pages/Seller/Dashboard/SellerOrders.tsx
@@ -2,12 +2,22 @@ import * as React from "react";
 import { Order } from "../../../components/shared/types/Order";
 import { PLACEHOLDER_ORDERS } from "../../../constants/placeholder-data";
 import DisplayedCustomerOrderItem from "../../Buyer/DisplayedCustomerOrderItem";
+import {useAPIHook} from "../../../components/shared/hooks/use-api-hook.ts";
 
-const SellerOrders: React.FC<{}> = (props) => {
+const SellerOrders: React.FC = () => {
     const [orders, setOrders] = React.useState<Order[]>(PLACEHOLDER_ORDERS);
-
-    // TODO: Fetch orders from our API
-
+    const apiHook = useAPIHook();
+    React.useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                const response = await apiHook.get(`http://localhost:3000/api/orders?itemOwnerId=65f8eb04ccc06864028d51f6`);
+                setOrders(response.data.orders);
+            } catch (error) {
+                console.error('Error fetching orders:', error);
+            }
+        };
+        fetchOrders();
+    }, [apiHook]);
     return (
         <div className="subcontent">
             <h3 className="header">Ongoing Orders</h3>
@@ -20,4 +30,4 @@ const SellerOrders: React.FC<{}> = (props) => {
         </div>
     );
 }
-export default SellerOrders
+export default SellerOrders;
