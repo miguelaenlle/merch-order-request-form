30722324091a442eab227b0a70838c74ca549874
no <109254246+Da-nu@users.noreply.github.com>
2024-04-25 20:03:58 -0500

    oh my goodness i forgot to make a route for upgrading buyers to sellers

--- backend/src/routes/auth-routes.ts
+++ backend/src/routes/auth-routes.ts
@@ -1,6 +1,15 @@
 import { Router } from 'express';
-import { loginUser, createUser, confirmEmail, resendConfirmationEmail, postReset, postNewPassword } from '../controllers/auth-controller';
+import {
+    loginUser,
+    createUser,
+    confirmEmail,
+    resendConfirmationEmail,
+    postReset,
+    postNewPassword,
+    upgradeToSeller
+} from '../controllers/auth-controller';
 import { body } from 'express-validator';
+import {auth, authExtended} from "../middleware/auth";
 
 const router = Router()
 
@@ -33,5 +42,9 @@ router.post('/new-password', [
     body('password').isString().withMessage('Password must be valid').notEmpty().withMessage('Password is required'),
     body('passwordToken').isString().withMessage('Password token must be valid').notEmpty().withMessage('Password token is required')
 ], postNewPassword);
+
+router.post('/upgrade-to-seller', [
+    body('email').isEmail().withMessage('Email must be valid').notEmpty().withMessage('Email is required')
+], auth, authExtended, upgradeToSeller);
 
 export default router;
