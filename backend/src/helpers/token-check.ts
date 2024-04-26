14be96290c88d9f9997b3a307aacf05382580186
no <109254246+Da-nu@users.noreply.github.com>
2024-04-25 20:08:19 -0500

    it has been done

--- backend/src/helpers/token-check.ts
+++ backend/src/helpers/token-check.ts
@@ -1,5 +1,5 @@
 import {JwtPayload} from "jsonwebtoken";
 
 export const tokenCheck = async (token: JwtPayload, type: string = "login") => {
-        return token.type == type;
+        return token.type == type; //world's most useful line of code
 }
