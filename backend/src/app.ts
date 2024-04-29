cde41e2f4b07cf12dce6520c7e5ceb7d86df1443
AAlchi <alialchi07@gmail.com>
2024-04-29 10:29:03 -0500

    Changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,11 +7,13 @@ import inventoryItem from "./routes/inventory-item-routes";
 import orderRoutes from "./routes/order-routes";
 import itemRoutes from "./routes/items-routes";
 import cors from "cors";
+import filterItem from './routes/filter-item-routes'
 
 const app: Application = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 app.use(cors());
 app.options("*", cors());
 
@@ -21,6 +23,7 @@ app.use('/api/dummies', dummyRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/groups', groupRoutes);
 app.use('/api/images', imageRoutes);
+app.use('/api/items/search', filterItem)
 app.use('/api/items', itemRoutes);
 app.use('/api/inventory-items', inventoryItem)
 app.use('/api/orders', orderRoutes)
